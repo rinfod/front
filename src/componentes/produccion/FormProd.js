3b03fbe81acf05e88ddb98b596c351f3f0d8f709
rolando <rolando.pedro.gabriel@gmail.com>
2025-01-18 15:55:21 -0600

    todo

---
+++ src/componentes/produccion/FormProd.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import '../../template/secctions/card.css'
+
+const FormProd = ({ onProductoActualizado }) => {
+
+    const [formData, setFormData] = useState({
+        tipo: '',
+        nombre: '',
+        resumen: '',
+        autor: '',
+      });
+    
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+      };
+    
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        try {
+          const response = await axios.post('http://localhost:5000/prod/producciones', formData);
+          alert('Registrado con éxito');
+          setFormData({
+            tipo: '',
+            nombre: '',
+            resumen: '',
+            autor: '',
+          })
+          // Notifica al padre que debe actualizar los productos
+      onProductoActualizado();
+        } catch (error) {
+          console.error(error.response?.data || 'An error occurred');
+          alert('Error registering user');
+        }
+      };
+
+    return(
+        <div class="card-prod">
+    <h1>Registro de Productos Científicos</h1>
+    <form onSubmit={handleSubmit}>
+      <div class="form-group">
+        <label for="region">Tipo</label>
+        <input type="text" id="tipo" name="tipo" placeholder="Ingresa el Tipo" value={formData.tipo}
+            onChange={handleChange}
+            required />
+      </div>
+      <div class="form-group">
+        <label for="distrito">Nombre</label>
+        <input type="text" id="nombre" name="nombre" placeholder="Ingresa el nombre" value={formData.nombre}
+            onChange={handleChange} required />
+      </div>
+      <div class="form-group">
+        <label for="descripcion">Resumen</label>
+        <input type="text" id="resumen" name="resumen" placeholder="Ingresa el resumen" value={formData.resumen}
+            onChange={handleChange} required />
+      </div>
+      <div class="form-group">
+        <label for="descripcion">Autor</label>
+        <input type="text" id="autor" name="autor" placeholder="Ingresa el autor" value={formData.autor}
+            onChange={handleChange} required />
+      </div>
+      <button type="submit" class="btn">Enviar</button>
+    </form>
+  </div>
+    );
+};
+
+export default FormProd;
--- src/componentes/produccion/Produccion.js
+++ src/componentes/produccion/Produccion.js
@@ -1,111 +1,192 @@
-const Produccion = () =>{
-    return(
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import FormProd from "./FormProd";
+
+
+import './producto.css'; // Archivo CSS para estilos
+
+const Produccion = () => {
+    const [produccion, setProduccion] = useState([]);
+
+    // Obtener los productos desde el servidor
+  const fetchProductos = async () => {
+    axios.get('http://localhost:5000/prod/produccion')
+            .then(response => {
+                setProduccion(response.data);
+            })
+            .catch(error => {
+                console.error('Error al obtener los productos:', error);
+            });
+  };
+
+    useEffect(() => {
+       fetchProductos();
+    }, []);
+
+
+    return (
         <div>
-        <div class="content">
-            <div class="container padding-y-big">
-                <div class="row">
-                    <div class="col col-lg-12 col-sm-12">
-                        <h2 className="fw500 tertiary-color no-margin" >Artículos</h2>
-                        <p class="fs-mid">
-                            <strong>
-                                
-                            </strong>
-                        </p>
+
+            <div class="content padding-y-big tertiary-bg-mapa">
+                <div class="container padding-y-big">
+                    <div class="row">
+
+                        <div class="col col-lg-12 col-sm-12">
+                            <FormProd onProductoActualizado={fetchProductos} />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
+            <div class="ccontent sub-footer header-bg md-hide">
+                <div class="container padding-y">
 
+                    <div class="row">
+                        <div class="col col-lg-12 col-sm-12 text-col">
+                            ¡Productos <span class="secondary-color"></span> Científicos!
+                        </div>
 
-        <div class="content padding-y-big tertiary-bg-mapa">
-            <div class="container padding-y-big">
-                <div class="row">
-
-                    <div class="col col-lg-12 col-sm-12">
-                        <h2 className="fw500 white-color no-margin" >Libros y Capítulos</h2>
-                        <p class="fs-mid">
-                            <strong>
-                             
-                            </strong>
-                        </p>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <div class="content">
-            <div class="container padding-y-big">
-                <div class="row">
-                    <div class="col col-lg-12 col-sm-12">
-                        <h2 className="fw500 tertiary-color no-margin" >Infografías</h2>
-                        <p class="fs-mid">
-                            <strong>
-                                
-                            </strong>
-                        </p>
+            <div class="content padding-y-big tertiary-bg-mapa">
+                <div class="container padding-y-big">
+                    <div class="row">
+                        <div class="col col-lg-12 col-sm-12">
+
+
+                            <div className="productos-container">
+                                {produccion.map(producto => (
+                                    <div key={producto._id} className="card">
+                                        <h3 >{producto.tipo}</h3>
+                                        <hr className='thin-gray'/>
+                                        <p>{producto.nombre}</p>
+                                        <hr className='thin-gray'/>
+                                        <p>{producto.resumen}</p>
+                                        <hr className='thin-gray'/>
+                                        <p><strong>Autor:</strong> {producto.autor}</p>
+                                    </div>
+                                ))}
+                            </div>
+
+
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
 
 
-        <div class="content padding-y-big tertiary-bg-mapa">
-            <div class="container padding-y-big">
-                <div class="row">
+            
 
-                    <div class="col col-lg-12 col-sm-12">
-                        <h2 className="fw500 white-color no-margin" >Informes</h2>
-                        <p class="fs-mid">
-                            <strong>
-                               
-                            </strong>
-                        </p>
+            {/*
+            <div class="content">
+                <div class="container padding-y-big">
+                    <div class="row">
+                        <div class="col col-lg-12 col-sm-12">
+                            <h2 className="fw500 tertiary-color no-margin" >Artículos</h2>
+                            <p class="fs-mid">
+                                <strong>
+
+                                </strong>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
 
-        <div class="content">
-            <div class="container padding-y-big">
-                <div class="row">
-                    <div class="col col-lg-12 col-sm-12">
-                        <h2 className="fw500 tertiary-color no-margin" >Reportes de Caso</h2>
-                        <p class="fs-mid">
-                            <strong>
-                                
-                            </strong>
-                        </p>
+
+            <div class="content padding-y-big tertiary-bg-mapa">
+                <div class="container padding-y-big">
+                    <div class="row">
+
+                        <div class="col col-lg-12 col-sm-12">
+                            <h2 className="fw500 white-color no-margin" >Libros y Capítulos</h2>
+                            <p class="fs-mid">
+                                <strong>
+
+                                </strong>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
+            <div class="content">
+                <div class="container padding-y-big">
+                    <div class="row">
+                        <div class="col col-lg-12 col-sm-12">
+                            <h2 className="fw500 tertiary-color no-margin" >Infografías</h2>
+                            <p class="fs-mid">
+                                <strong>
 
-
-        <div class="content padding-y-big tertiary-bg-mapa">
-            <div class="container padding-y-big">
-                <div class="row">
-
-                    <div class="col col-lg-12 col-sm-12">
-                        <h2 className="fw500 white-color no-margin" >Cartogramas</h2>
-                        <p class="fs-mid">
-                            <strong>
-                               
-                            </strong>
-                        </p>
+                                </strong>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
+
+
+
+            <div class="content padding-y-big tertiary-bg-mapa">
+                <div class="container padding-y-big">
+                    <div class="row">
+
+                        <div class="col col-lg-12 col-sm-12">
+                            <h2 className="fw500 white-color no-margin" >Informes</h2>
+                            <p class="fs-mid">
+                                <strong>
+
+                                </strong>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="content">
+                <div class="container padding-y-big">
+                    <div class="row">
+                        <div class="col col-lg-12 col-sm-12">
+                            <h2 className="fw500 tertiary-color no-margin" >Reportes de Caso</h2>
+                            <p class="fs-mid">
+                                <strong>
+
+                                </strong>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="content padding-y-big tertiary-bg-mapa">
+                <div class="container padding-y-big">
+                    <div class="row">
+
+                        <div class="col col-lg-12 col-sm-12">
+                            <h2 className="fw500 white-color no-margin" >Cartogramas</h2>
+                            <p class="fs-mid">
+                                <strong>
+
+                                </strong>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+*/}
+
+
+
         </div>
-
-
-
-
-
-    </div>
     );
 }
 
